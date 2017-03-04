e675a8f7a613d94a480656439d528422992c3cc3
beepscore <support@beepscore.com>
2017-03-04 13:08:10 -0800

    In bus.py extract functions

--- bus/bus.py
+++ bus/bus.py
@@ -14,17 +14,46 @@ import webbrowser
 url_string = 'http://ctabustracker.com/bustime/map/getBusesForRoute.jsp?route=22'
 filename = 'data/output/rt22.xml'
 
+
 def download_buses(url_string, outfilename):
     u = urllib.request.urlopen(url_string)
     data = u.read()
 
     f = open(outfilename, 'wb')
     f.write(data)
-    # 4431
     f.close()
 
+
+def bus_id(bus):
+    """ given bus xml, returns id as text"""
+
+    id_text = bus.find('id').text
+    return id_text
+
+
+def bus_is_northbound(bus):
+    """ given bus xml, returns True if bus is Northbound"""
+
+    dd = bus.find('dd').text
+    return  dd is 'Northbound'
+
+
+def bus_latitude(bus):
+    """ given bus xml, returns latitude"""
+
+    latitude = float(bus.find('lat').text)
+    return latitude
+
+
+def bus_longitude(bus):
+    """ given bus xml, returns longitude"""
+
+    longitude = float(bus.find('lon').text)
+    return longitude
+
+
 def find_buses(filename):
-    # finds buses meeting criteria
+    """ finds buses meeting criteria """
 
     # parse xml into a document tree
     doc = parse(filename)
@@ -41,14 +70,14 @@ def find_buses(filename):
     daves_longitude = -87.668452
 
     for bus in buses:
-        dd = bus.find('dd').text
-        is_northbound = dd is 'Northbound'
-        latitude = float(bus.find('lat').text)
-        longitude = float(bus.find('lon').text)
+        latitude = bus_latitude(bus)
+        longitude = bus_longitude(bus)
+
         if latitude >= daves_latitude:
             # bus id 4068 41.99755483598852
-            print('bus id', bus.find('id').text, latitude, 'dd', dd)
+            print('bus id', bus_id(bus), latitude)
 
             # f requires python >= 3.6
             markers = f"&markers={latitude}, {longitude}"
             webbrowser.open(map_url_start + markers)
+
