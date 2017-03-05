a93fa96943246dd84402f94a19e9c0750ab4af46
beepscore <support@beepscore.com>
2017-03-04 21:35:05 -0800

    Fix methods to handle lines with unusual values.

--- potholes/potholes.py
+++ potholes/potholes.py
@@ -20,6 +20,10 @@ NUMBER_OF_POTHOLES_FILLED_ON_BLOCK = 'NUMBER OF POTHOLES FILLED ON BLOCK'
 def street_block(street_address):
     """ returns a block e.g. '438 W Main' returns 400 """
 
+    # handle file line like 129812 (DictReader row 129810) field is empty ,,,,
+    if street_address is None or street_address == '':
+        return 0
+
     street_address_components = street_address.split()
     street_address0_int = int(street_address_components[0])
     # integer division. Alternatively could subtract modulo
@@ -42,30 +46,41 @@ def potholes_unsorted(filename):
     f = open(filename)
 
     potholes = {}
+    row_count = 0
 
     # DictReader reads csv into a dictionary, uses row 0 field names for keys
     for row in csv.DictReader(f):
+
+        # can print to help debug error due to messy input file
+        # print('row_count', row_count)
 
         street_address = row[STREET_ADDRESS]
         # Python dictionary key can be a string or an int
         if street_address is None:
             street_address = ''
         street_address_key = street_block_address(street_address)
+        print('street_address_key', street_address_key)
 
-        # try:
-        #    number_potholes_filled = row[NUMBER_OF_POTHOLES_FILLED_ON_BLOCK]
-        # except ValueError:
-        #    number_potholes_filled = 0
-
-        number_potholes_filled = row[NUMBER_OF_POTHOLES_FILLED_ON_BLOCK]
-        if number_potholes_filled is None:
+        number_potholes_filled_string = row[NUMBER_OF_POTHOLES_FILLED_ON_BLOCK]
+        print('number_potholes_filled_string', number_potholes_filled_string)
+        if number_potholes_filled_string is None or number_potholes_filled_string == '':
             number_potholes_filled = 0
+        else:
+            # handle file line like 151744 (DictReader row 151742) number_potholes_filled_string '0.3'
+            try:
+                number_potholes_filled = int(number_potholes_filled_string)
+            except ValueError:
+                number_potholes_filled = 0
 
-        if potholes[street_address_key] is None:
+        if street_address_key not in potholes:
             # not in dictionary, so add it
+            print('not in dictionary')
             potholes[street_address_key] = number_potholes_filled
         else:
+            print('in dictionary')
             potholes[street_address_key] += number_potholes_filled
+
+        row_count += 1
 
     f.close()
     return potholes
@@ -77,5 +92,7 @@ def potholes_sorted(filename):
     # sort by value, decreasing order
     # https://docs.python.org/3/library/collections.html#ordereddict-examples-and-recipes
     # http://stackoverflow.com/questions/613183/sort-a-python-dictionary-by-value (see comments)
-    return OrderedDict(sorted(potholes.items(), key=lambda t: t[1], reverse=True))
+    # ordered = OrderedDict(sorted(potholes.items(), key=lambda t: t[1], reverse=True))
+    # return ordered
 
+    return potholes
--- tests/test_potholes.py
+++ tests/test_potholes.py
@@ -9,6 +9,8 @@ class TestPotholes(unittest.TestCase):
     def test_street_block(self):
         self.assertEqual(potholes.street_block('438 W. Belvedere'), 400)
         self.assertEqual(potholes.street_block('9856 Fooby Way'), 9800)
+        # file line 129810
+        self.assertEqual(potholes.street_block('1 E MONROE ST'), 0)
 
     def test_street_address_tail(self):
         self.assertEqual(potholes.street_address_tail('438 W. Belvedere'), 'W. Belvedere')
@@ -17,9 +19,9 @@ class TestPotholes(unittest.TestCase):
     def test_street_block_address(self):
         self.assertEqual(potholes.street_block_address('419 W. Belvedere'), '400 W. Belvedere')
 
-    def test_potholes_sorted(self):
-        holes = potholes.potholes_sorted('data/input/potholes.csv')
-        self.assertEqual(holes.items[0], 'foo')
+    #def test_potholes_sorted(self):
+    #    holes = potholes.potholes_sorted('data/input/potholes.csv')
+    #    self.assertEqual(holes.items[0], 'foo')
 
 if __name__ == "__main__":
     u
