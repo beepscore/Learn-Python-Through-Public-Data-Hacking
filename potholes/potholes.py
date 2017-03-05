6efa361f6b52636323bd488794dc0ef9f25ba526
beepscore <support@beepscore.com>
2017-03-04 17:52:37 -0800

    Revise methods.

--- potholes/potholes.py
+++ potholes/potholes.py
@@ -16,17 +16,51 @@ import csv
 STREET_ADDRESS = 'STREET ADDRESS'
 NUMBER_OF_POTHOLES_FILLED_ON_BLOCK = 'NUMBER OF POTHOLES FILLED ON BLOCK'
 
-def street_address_trimmed(street_address):
-    address_number = int(street_address.split()[0])
-    return address_number // 100
+def street_block(street_address):
+    """ returns a block e.g. '438 W Main' returns 400 """
 
-def populate_potholes():
+    street_address_components = street_address.split()
+    street_address0_int = int(street_address_components[0])
+    # integer division. Alternatively could subtract modulo
+    return (street_address0_int // 100) * 100
+
+
+def street_address_tail(street_address):
+    street_address_components = street_address.split()
+    # discard component[0]
+    tail_slice = street_address_components[1:]
+    # Python uses join(list), not list.join() so join can be used on any iterable
+    return ' '.join(tail_slice)
+
+
+def street_block_address(street_address):
+    street_address_components = street_address.split()
+    return str(street_block(street_address)) + ' ' + street_address_tail(street_address)
+
+
+def potholes_unsorted():
     f = open('data/input/potholes.csv')
+
+    potholes = {}
+
     # DictReader reads csv into a dictionary, uses row 0 field names for keys
     for row in csv.DictReader(f):
 
         street_address = row[STREET_ADDRESS]
+        # Python dictionary key can be a string or an int
+        street_address_key = street_block_address(street_address)
+
         try:
             number_potholes_filled = row[NUMBER_OF_POTHOLES_FILLED_ON_BLOCK]
         except ValueError:
             number_potholes_filled = 0
+
+        if potholes[street_address_key] is None:
+            # not in dictionary, so add it
+            potholes[street_address_key] = number_potholes_filled
+        else:
+            potholes[street_address_key] += number_potholes_filled
+
+    return potholes
+
+
--- tests/test_potholes.py
+++ tests/test_potholes.py
@@ -8,10 +8,18 @@ class TestPotholes(unittest.TestCase):
 
     # street_address_trimmed
 
-    def test_street_address_trimmed(self):
-        self.assertEqual(potholes.street_address_trimmed('438 W. Belvedere'), 4)
-        self.assertEqual(potholes.street_address_trimmed('9856 Fooby Way'), 98)
+    def test_street_block(self):
+        self.assertEqual(potholes.street_block('438 W. Belvedere'), 400)
+        self.assertEqual(potholes.street_block('9856 Fooby Way'), 9800)
 
+
+    def test_street_address_tail(self):
+        self.assertEqual(potholes.street_address_tail('438 W. Belvedere'), 'W. Belvedere')
+        self.assertEqual(potholes.street_address_tail('9856 Fooby Way'), 'Fooby Way')
+
+
+    def test_street_block_address(self):
+        self.assertEqual(potholes.street_block_address('419 W. Belvedere'), '400 W. Belvedere')
 
 if __name__ == "__main__":
     u
