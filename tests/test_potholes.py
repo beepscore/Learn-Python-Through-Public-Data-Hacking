926bbe100fe77ac7636083de058cb1ec1d9f9208
beepscore <support@beepscore.com>
2017-03-04 17:00:49 -0800

    In test_street_address_trimmed add assertion.

--- tests/test_potholes.py
+++ tests/test_potholes.py
@@ -10,6 +10,7 @@ class TestPotholes(unittest.TestCase):
 
     def test_street_address_trimmed(self):
         self.assertEqual(potholes.street_address_trimmed('438 W. Belvedere'), 4)
+        self.assertEqual(potholes.street_address_trimmed('9856 Fooby Way'), 98)
 
 
 if __name__ == "__main__":
